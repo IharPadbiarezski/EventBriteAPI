2f2dd2b05481c90ae34e54f5410df1eaff7ca079
IharPadbiarezski <yb.igrok.by@gmail.com>
2019-05-12 14:24:44 +0300

    feat: print error message

--- js/app.js
+++ js/app.js
@@ -2,3 +2,24 @@
 
 const eventbrite = new EventBrite();
 const ui = new UI();
+
+// Listener for the submit button
+document.getElementById("submitBtn").addEventListener("click", e => {
+  e.preventDefault();
+
+  // get values from form
+  const eventName = document.getElementById("event-name").value;
+  const category = document.getElementById("category").value;
+
+  //   console.log(eventName + " : " + category);
+
+  if (eventName !== "") {
+    // Query Event Brite API
+  } else {
+    // Print a message
+    ui.printMessage(
+      "Add an Event or City",
+      "text-center alert alert-danger mt-4"
+    );
+  }
+});
--- js/ui.js
+++ js/ui.js
@@ -26,4 +26,30 @@ class UI {
       })
       .catch(error => console.log(error));
   }
+
+  // Displays a message
+  printMessage(message, className) {
+    // Create a div
+    const div = document.createElement("div");
+    div.className = className;
+    // Add the Text
+    div.appendChild(document.createTextNode(message));
+
+    // Insert intothe HTML
+    const searchDiv = document.querySelector("#search-events");
+    searchDiv.appendChild(div);
+
+    // Remove the alert after 3 seconds
+    setTimeout(() => {
+      this.removeMessage();
+    }, 3000);
+  }
+
+  // Remove the message
+  removeMessage() {
+    const alert = document.querySelector(".alert");
+    if (alert) {
+      alert.remove();
+    }
+  }
 }
